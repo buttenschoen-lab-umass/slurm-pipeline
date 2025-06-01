203cd4b4b4f4853dfca9649f034b310bd4a24496
Andreas Buttenschoen <andreas@buttenschoen.ca>
2025-06-01 18:58:31 -0400

    Let's actually use the proper structure.

--- core/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Core simulation pipeline components.
-"""
-
-from .pipeline import SimulationPipeline, SimulationResult
-from .ensemble import Ensemble
-from .parameter_scan import ParameterScan
-
-__all__ = ["SimulationPipeline", "SimulationResult", "Ensemble", "ParameterScan"]
--- diagnostics/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Diagnostic tools for SLURM pipeline debugging.
-"""
-
-from .debugger import SlurmDebugger
-
-__all__ = ["SlurmDebugger"]
